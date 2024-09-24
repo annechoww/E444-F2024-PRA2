cfce16dc1331c3cba6a78fcffa94c46bcb714b43
annechoww <anne.chow123@hotmail.com>
2024-09-24 18:29:17 -0400

    4.1-4.2: Added secret key config and form class definition

--- hello.py
+++ hello.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'i bet YOU cannot guess this secret key'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def index():
