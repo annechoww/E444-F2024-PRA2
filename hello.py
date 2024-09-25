ad4a7d31bb298e5010cf47bd550d9a5ce160bef8
annechoww <anne.chow123@hotmail.com>
2024-09-24 20:57:03 -0400

    Added email display and UofT Check. Added flash message/session for email changes.

--- hello.py
+++ hello.py
@@ -26,11 +26,15 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.name.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 
 @app.route('/user/<name>')
